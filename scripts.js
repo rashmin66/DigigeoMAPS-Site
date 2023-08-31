4b09157f6cc0be3d52e50733b4c6a4cbcdf3244f
unknown <hansi20ra@gmail.com>
2023-08-31 21:32:39 +0530

    final changes with responsive

--- scripts.js
+++ scripts.js
@@ -166,3 +166,31 @@ function showNavPopUp2() {
     overlay.style.display = "block";
 }
 
+//function for tree view
+$(function () {
+    $(".tree li:has(ul)")
+        .addClass("parent_li")
+        .find(" > span")
+        .attr("title", "Collapse this branch");
+    $(".tree li.parent_li > span").on("click", function (e) {
+        var children = $(this).parent("li.parent_li").find(" > ul > li");
+        if (children.is(":visible")) {
+            children.hide("fast");
+            $(this)
+                .attr("title", "Expand this branch")
+                .find(" > i")
+                .addClass("fas fa-angle-right")
+                .removeClass("fas fa-angle-down");
+        } else {
+            children.show("fast");
+            $(this)
+                .attr("title", "Collapse this branch")
+                .find(" > i")
+                .addClass("fas fa-angle-down")
+                .removeClass("fas fa-angle-right");
+        }
+        e.stopPropagation();
+    });
+});
+
+
